2fa41ae554f911c1eb837bf9a6c26ede46e16e0b
weerawi <weerawikramatharindu@gmail.com>
2023-05-16 21:38:03 +0530

    created auto suggestion and background change

---
+++ src/Result/Result.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Names of the props we expect to receive
+const keys = ['title', 'description', 'price', 'rating', 'category'];
+
+const Result = (props) => (
+  <div>
+    {keys.map((key) => (
+      <span>{key.charAt(0) + key.slice(1)}: {props[key]}</span>
+    )}
+  </div>
+);
+
+export default Result;
--- src/Search/SearchBar.jsx
+++ src/Search/SearchBar.jsx
@@ -1,9 +1,62 @@
+import { useEffect, useState } from "react";
 import "./SearchBar.css";
+import axios from "axios";
 
 const SearchBar = () => {
+  const url = "https://dummyjson.com/products/search?q";
+
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [hideSuggestions, setHideSuggestions] = useState(true);
+  const [result, setResult] = useState(null);
+
+  const findResult = (title) => {
+    setResult(suggestions.find((suggestion) => suggestion.title === title));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${url}=${value}`);
+        setSuggestions(data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [value]);
+
   return (
-    <div className="searchbar-container">
-      <input type="text" className="textbox" placeholder="Search..."></input>
+    <div className="container">
+      <input
+        type="text"
+        className="textbox"
+        value={value}
+        placeholder="Search..."
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        onFocus={() => setHideSuggestions(false)}
+        onBlur={async () => {
+          setTimeout(() => {
+            setHideSuggestions(true);
+          }, 200);
+        }}
+      ></input>
+
+      <div
+        className={`suggestions  ${hideSuggestions && ["heightsearch hidden"]}`}
+      >
+        {suggestions.map((suggestion) => (
+          <div
+            className="suggestion"
+            onClick={() => findResult(suggestion.title)}
+          >
+            {suggestion["title"]}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
